581b297ebc2582d6b7c69242a7eb3194ff50d7fd
XxxxxYyyyy <v-bruhal@microsoft.com>
2019-09-13 10:17:31 +0000

    Automated push from build 78827

---
+++ 21.customization-plain-ui/precache-manifest.2c6fdfaa8c423bb7a6c185b42c1a1ae0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48929c8b7102a431fd77d44b6b535818",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/index.html"
+  },
+  {
+    "revision": "6c6cca3c3c98d3d0c60b",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/static/js/2.df92e261.chunk.js"
+  },
+  {
+    "revision": "13874203c265218296a4",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/static/js/main.6599cf4c.chunk.js"
+  },
+  {
+    "revision": "607f44fcff51377f6521",
+    "url": "/BotFramework-WebChat/21.customization-plain-ui/static/js/runtime~main.6c454de8.js"
+  }
+]);
